05cbad878fe10528e519b2cc66c0fed51faf5257
Chuan Wang <chuan.wang101@gmail.com>
2022-05-30 16:46:14 -0700

    finish projects page

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -23,9 +23,9 @@ export default function PortfolioContainer() {
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
-    <div>
+    <>
       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {renderPage()}
-    </div>
+    </>
   );
 }
