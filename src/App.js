d77c80a22c51cdd875152bb23c4d27fa8e16365c
Chuan Wang <chuan.wang101@gmail.com>
2022-05-30 18:17:10 -0700

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PortfolioContainer from './components/PortfolioContainer';
+import Footer from './components/Footer';
 
 function App() {
   return(
     <>
       <PortfolioContainer />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <a href="mailto:chuan.wang101@gmail.com"> chuan.wang101@gmail.com </a>
+            <a href="https://github.com/chuanw101/" target="_blank" rel="noreferrer"> GitHub </a>
+            <a href="https://www.linkedin.com/in/chuan-wang-25842694/" target="_blank" rel="noreferrer"> LinkedIn </a>
+        </footer>
+    )
+}
